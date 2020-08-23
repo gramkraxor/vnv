22c51c56ce9b95018e6131316437457866f09c63
Gramkraxor <gram@krax.dev>
2020-08-23 00:03:29 -0500

    Drop `local` for shell compatibility
    Ksh doesn't support it.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = Path('README.md').read_text()
 # https://packaging.python.org/guides/distributing-packages-using-setuptools/
 setuptools.setup(
     name='vnv',
-    version='0.1.1',
+    version='0.1.2',
     author='Gramkraxor',
     author_email='gram@krax.dev',
     url='https://github.com/gramkraxor/vnv',
