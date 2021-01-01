3b5c59ce251918311bd535c7b3a66251c60e1180
Gramkraxor <gram@krax.dev>
2020-12-31 23:04:26 -0600

    Increment version number to 1.0
    Is this update One-Point-Oh material? No. But do its breaking changes
    call for a major release? Yes.
    
    Also, it's a small project, so I'm switching to a 2-part version scheme.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = Path('README.md').read_text()
 # https://packaging.python.org/guides/distributing-packages-using-setuptools/
 setuptools.setup(
     name='vnv',
-    version='0.1.4',
+    version='1.0',
     author='Gramkraxor',
     author_email='gram@krax.dev',
     url='https://github.com/gramkraxor/vnv',
