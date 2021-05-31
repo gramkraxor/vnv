8acf0800d97acc7ab6c7f39dd8c629066f0bd0e9
Gramkraxor <gram@krax.dev>
2021-05-31 17:13:53 -0500

    Single-source the package version

--- setup.py
+++ setup.py
@@ -4,10 +4,8 @@ import setuptools
 
 long_description = Path('README.md').read_text()
 
-# https://packaging.python.org/guides/distributing-packages-using-setuptools/
 setuptools.setup(
     name='vnv',
-    version='1.2',
     author='Gramkraxor',
     author_email='gram@krax.dev',
     url='https://github.com/gramkraxor/vnv',
