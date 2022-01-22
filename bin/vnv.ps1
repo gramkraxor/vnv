21d84bb3846f3bde6e39baeb9727f1ad34cf15e8
Gramkraxor <gram@krax.dev>
2022-01-22 14:29:56 -0600

    Use forward slash in `vnv.ps1`

--- bin/vnv.ps1
+++ bin/vnv.ps1
@@ -1,7 +1,7 @@
 # vnv, the little shortcut for virtualenv.
 # Wrapper script for PowerShell.
 
-$VNV_FINISH = $env:USERPROFILE + "\.vnv-finish.ps1"
+$VNV_FINISH = $env:USERPROFILE + "/.vnv-finish.ps1"
 vnv.cli powershell $Args
 if ($? -and (Test-Path -LiteralPath $VNV_FINISH -PathType Leaf)) {
     & $VNV_FINISH
